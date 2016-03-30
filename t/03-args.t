garu/Net-Google-Analytics-MeasurementProtocol

commit ed09f1fb5cc909cd6f7cb71799a520dcb8fc760e

Allow the user to pass an arbitrary useragent object.
The purpose behind this is to make it easy to mock network calls in a
test environment.  This is very easy to do with something like
Test::LWP::UserAgent, but more difficult to do with Furl.  The
difference between the two classes is one argument to the post() method.
Furl needs the headers passes as an undef and with LWP::UserAgent, you
can just omit the headers entirely.

lib/Net/Google/Analytics/MeasurementProtocol.pm
@@ -24,11 +24,16 @@ sub new {
     $args{an}  ||= 'My App';
     $args{ds}  ||= 'app';
 
+    my $ua_object = $args{ua_object} || _build_user_agent( $args{ua} );
+    unless ( $ua_object->isa('Furl') || $ua_object->isa('LWP::UserAgent') ) {
+        Carp::croak('ua_object must be of type Furl or LWP::UserAgent');
+    }
+
     my $debug = delete $args{debug};
     return bless {
         args  => \%args,
         debug => $debug,
-        ua    => _build_user_agent( $args{ua} ),
+        ua    => $ua_object,
     }, $class;
 }
 
@@ -67,9 +72,14 @@ sub _request {
         : 'https://www.google-analytics.com/collect'
         ;
 
-    my $res = $ua->post( $target, undef, $args );
-
-    return $self->{debug} ? $res : $res->is_success;
+    # Compatibility layer for LWP::UserAgent
+    my $res = $ua->post( $target, $ua->isa('Furl') ? undef : (), $args );
+    if ($self->{debug}) {
+        return $res;
+    }
+    else {
+        return $self->{debug} ? $res : $res->is_success;
+    }
 }
 
 sub _build_user_agent {

t/02-hits.t
@@ -17,32 +17,53 @@ else {
     diag q(JSON looks missing. We'll make due with regexes);
 }
 
+my $has_lwp = eval {
+    require LWP::UserAgent;
+    require LWP::Protocol::https;
+    1;
+};
+
+test_pageview();
+
+SKIP: {
+    skip 'LWP::Protocol::https is likely not installed', 2, unless $has_lwp;
+    test_pageview( LWP::UserAgent->new );
+};
+
+sub test_pageview {
+    my $ua_object = shift;
+    diag 'Using ' . ( $ua_object ? 'LWP::UserAgent' : 'Furl' );
+
+    my $ga = Net::Google::Analytics::MeasurementProtocol->new(
+        tid   => 'UA-1234-5',
+        debug => 1,
+        $ua_object ? ( ua_object => $ua_object ) : (),
+    );
+
+
+    my $res = $ga->send( 'pageview', {
+        dh => 'www.colab55.com',
+        dp => '/pop',
+        dt => '/pop',
+    });
+
+    if ($has_json) {
+        my $json = JSON::decode_json( $res->decoded_content );
+        ok exists $json->{hitParsingResult}[0]{valid}, 'valid response fields';
+        is "$json->{hitParsingResult}[0]{valid}", 0, 'invalid response is invalid';
+    }
+    else {
+        like $res->decoded_content, qr/"valid": false/s, 'invalid response is invalid';
+    }
+
+}
+
 my $ga = Net::Google::Analytics::MeasurementProtocol->new(
-    tid   => 'UA-1234-5',
-    debug => 1,
-);
-
-my $res = $ga->send( 'pageview', {
-    dh => 'www.colab55.com',
-    dp => '/pop',
-    dt => '/pop',
-});
-
-if ($has_json) {
-    my $json = JSON::decode_json( $res->decoded_content );
-    ok exists $json->{hitParsingResult}[0]{valid}, 'valid response fields';
-    is "$json->{hitParsingResult}[0]{valid}", 0, 'invalid response is invalid';
-}
-else {
-    like $res->decoded_content, qr/"valid": false/s, 'invalid response is invalid';
-}
-
-$ga = Net::Google::Analytics::MeasurementProtocol->new(
     tid => 'UA-12345678-9',
     debug       => 1,
 );
 
-$res = $ga->send( 'pageview', {
+my $res = $ga->send( 'pageview', {
     dh => 'www.colab55.com',
     dp => '/pop',
 });

commit 9bf95f5ce8a8b26062d4b05f8e72f89adcb34969

Scrub ua_object param before POST to GA.
A bug was introduced in ed09f1fb5 where a ua_object param gets POSTed to
Google Analytics in the form of something like:
MM::WWW::Mechanize=HASH(0x107b0de8)  This doesn't seem to cause the
request to fail, but it really shouldn't be there. :)

lib/Net/Google/Analytics/MeasurementProtocol.pm
@@ -24,7 +24,7 @@ sub new {
     $args{an}  ||= 'My App';
     $args{ds}  ||= 'app';
 
-    my $ua_object = $args{ua_object} || _build_user_agent( $args{ua} );
+    my $ua_object = delete $args{ua_object} || _build_user_agent( $args{ua} );
     unless ( $ua_object->isa('Furl') || $ua_object->isa('LWP::UserAgent') ) {
         Carp::croak('ua_object must be of type Furl or LWP::UserAgent');
     }
@@ -38,6 +38,12 @@ sub new {
 }
 
 sub send {
+    my ($self, $hit_type, $args) = @_;
+
+    return $self->_request( $self->_build_request_args( $hit_type, $args ) );
+}
+
+sub _build_request_args {
     my ($self, $hit_type, $args) = @_;
 
     my %args = (%{$self->{args}}, %$args, t => $hit_type);
@@ -59,8 +65,7 @@ sub send {
     }
     Carp::croak('for "pageview" hit types you must set either "dl" or both "dh" and "dp"')
         if $hit_type eq 'pageview' && !($args{dl} || ($args{dh} && $args{dp}));
-
-    return $self->_request(\%args);
+    return \%args;
 }
 
 sub _request {

t/03-args.t
@@ -1,0 +1,19 @@
+use strict;
+use warnings;
+use Test::More tests => 2;
+
+use Furl;
+use Net::Google::Analytics::MeasurementProtocol;
+
+my $ga = Net::Google::Analytics::MeasurementProtocol->new(
+    tid       => 'UA-1234-5',
+    ua_object => Furl->new,
+);
+
+my $args = $ga->_build_request_args( 'transaction', { ti => 1 } );
+
+ok( !exists $args->{ua_object},
+    'ua_object is scrubbed from args before POST' );
+
+ok( !exists $args->{debug},
+    'debug param is scrubbed from args before POST' );